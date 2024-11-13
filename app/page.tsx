fe602f9ffc03966f33ecf9f362aba8feeaecc746
nwleedev <nw.lee@outlook.com>
2024-11-13 15:02:34 +0900

    feat: click to send notification example

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 // import LoginButton from "./_component/LoginButton";
 // import LogoutButton from "./_component/LogoutButton";
 
+import NotificationDemo from "@/components/notification";
+
 export default function Home() {
   return (
     <main>
       <h1>WOOZUDA</h1>
       {/* <LoginButton />
       <LogoutButton /> */}
+      <p>Welcome to woozuda</p>
+      <NotificationDemo />
     </main>
   );
 }
