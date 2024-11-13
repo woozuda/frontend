af4e35cf4225f416cb2232209fc46fe2d4ae525b
nwleedev <nw.lee@outlook.com>
2024-11-13 15:00:08 +0900

    feat: service worker & web-push packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  swSrc: "app/lib/sw.ts",
+  swDest: "public/sw.js",
+  exclude: [/\/api$/],
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withSerwist(nextConfig);
