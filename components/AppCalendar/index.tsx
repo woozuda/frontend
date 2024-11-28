a0186358b1f728aab68bc1f3eab5896a2f8b228a
nwleedev <nw.lee@outlook.com>
2024-11-28 20:38:15 +0900

    refactor: calendar components

--- components/AppCalendar/index.tsx
+++ components/AppCalendar/index.tsx
@@ -1,11 +1,19 @@
 import { ClassNameLibs } from "@/lib/utils";
-import { addMonths, format, startOfDay, subMonths } from "date-fns";
+import {
+  addMonths,
+  format,
+  setMonth,
+  startOfDay,
+  startOfYear,
+  subMonths,
+} from "date-fns";
 import { ko } from "date-fns/locale";
 import {
+  cloneElement,
   createContext,
   forwardRef,
-  MouseEventHandler,
   PropsWithChildren,
+  ReactNode,
   useContext,
   useMemo,
   useState,
@@ -14,37 +22,43 @@ import {
 import ArrowLeftSvg from "@/app/assets/icons/ArrowLeftSmall.svg";
 import ArrowRightSvg from "@/app/assets/icons/ArrowRightSmall.svg";
 import XMarkSvg from "@/app/assets/icons/XMark.svg";
-import { CalendarLibs, CalendarStageType } from "@/app/lib/calendar";
-import AppCalendarDay from "./Day";
+import { CalendarLibs } from "@/app/lib/calendar";
+import { MonthDay } from "@/app/lib/calendar/types";
+import Link from "next/link";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+
+export interface CalendarDayProps extends PropsWithChildren {
+  date: MonthDay;
+  className?: string;
+}
+
+export type CalendarDay = (props: CalendarDayProps) => ReactNode;
 
 interface AppCalendarContextType {
   date: Date;
-  selectedDate?: Date;
   close?: boolean;
   onChange?: (date: Date) => unknown;
-  onHeaderClick?: MouseEventHandler<HTMLElement>;
+  onMonthClick?: (month: number) => unknown;
   onNextClick: () => unknown;
   onPrevClick: () => unknown;
 }
 
 const AppCalendarContext = createContext<AppCalendarContextType>(null!);
 
-export interface AppCalendarHeaderProps {
-  hasClose: boolean;
-  date: Date;
-  onPrevClick: () => unknown;
-  onNextClick: () => unknown;
-  onClose?: () => unknown;
-}
-
 const AppCalendarHeader = () => {
-  const { date, close, onNextClick, onPrevClick, onHeaderClick } =
+  const { date, close, onNextClick, onPrevClick } =
     useContext(AppCalendarContext);
   const text = format(date, "yyyy년 MM월", { locale: ko });
 
   if (!close) {
     return (
-      <div className="flex justify-center p-1 h-[56px] max-w-[412px]">
+      <div className="flex justify-center p-1 h-[56px]">
         <div className="p-3 gap-x-3 flex items-center">
           <button
             onClick={() => onPrevClick()}
@@ -52,14 +66,9 @@ const AppCalendarHeader = () => {
           >
             <ArrowLeftSvg />
           </button>
-          <h3
-            className="text-sub3 text-app-gray-1000"
-            onClick={(event) => {
-              onHeaderClick?.(event);
-            }}
-          >
-            {text}
-          </h3>
+          <AppCalendarSheet>
+            <h3 className="text-sub3 text-app-gray-400">{text}</h3>
+          </AppCalendarSheet>
           <button
             onClick={() => onNextClick()}
             className="w-6 h-6 flex justify-center items-center text-app-gray-400"
@@ -71,7 +80,7 @@ const AppCalendarHeader = () => {
     );
   } else {
     return (
-      <div className="flex justify-between p-1 h-[56px] w-full items-center max-w-[412px]">
+      <div className="flex justify-between p-1 h-[56px] w-full items-center">
         <div className="w-10 h-10" />
         <div className="p-3 gap-x-3 flex items-center">
           <button
@@ -80,7 +89,9 @@ const AppCalendarHeader = () => {
           >
             <ArrowLeftSvg />
           </button>
-          <h3 className="text-sub3 text-app-gray-1000">{text}</h3>
+          <AppCalendarSheet>
+            <h3 className="text-sub3 text-app-gray-400">{text}</h3>
+          </AppCalendarSheet>
           <button
             onClick={() => onNextClick()}
             className="w-6 h-6 flex justify-center items-center text-app-gray-400"
@@ -88,117 +99,35 @@ const AppCalendarHeader = () => {
             <ArrowRightSvg />
           </button>
         </div>
-        <button className="w-10 h-10 flex justify-center items-center text-app-gray-400">
-          <XMarkSvg />
-        </button>
+        <Link href={"/home"}>
+          <button className="w-10 h-10 flex justify-center items-center text-app-gray-400">
+            <XMarkSvg />
+          </button>
+        </Link>
       </div>
     );
   }
 };
 
-const AppCalendarShortBody = () => {
-  const { date, selectedDate } = useContext(AppCalendarContext);
+export interface AppCalendarWeekDayProps {
+  className?: string;
+}
+
+const AppCalendarWeekDay = (props: AppCalendarWeekDayProps) => {
+  const { date } = useContext(AppCalendarContext);
   const weekDays = CalendarLibs.createWeekNames(date);
-  const array = CalendarLibs.createWeekDays(date);
-
-  // Temporary Dates
-  const now = useMemo(() => new Date(), []);
-  const diaries = useMemo(
-    () => [
-      new Date(2024, 10, 10),
-      new Date(2024, 10, 11),
-      new Date(2024, 10, 13),
-      new Date(2024, 10, 15),
-    ],
-    []
-  );
-  return (
-    <div className="flex flex-col w-full max-w-[412px] px-5 whitespace-nowrap min-w-[280px]">
-      <div className="flex items-center px-3 h-[37px]">
-        <div className="w-full py-2 px-3 flex justify-between items-center">
-          {weekDays.map((day) => {
-            return (
-              <div key={day} className="flex justify-center items-center">
-                <span className="text-app-gray-1100 text-body3">{day}</span>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-      <div className="relative w-full flex flex-col p-3 gap-y-2.5 rounded-xl">
-        <div className="absolute bg-gradient-to-r from-gradient-01-100 to-gradient-01-200 opacity-10 rounded-xl top-0 left-0 w-full h-full" />
-        <div className="py-1 flex justify-between relative">
-          {array.map((day) => {
-            const type = CalendarLibs.getCalendarType(day, {
-              now,
-              diaries,
-              selectedDate,
-              stage: CalendarStageType.DEFAULT,
-            });
-            return (
-              <AppCalendarDay.Default
-                day={day.date}
-                key={day.key}
-                type={type}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const AppCalendarBodyDefault = () => {
-  const { date, selectedDate } = useContext(AppCalendarContext);
-  const weekDays = CalendarLibs.createWeekNames(date);
-  const { matrix } = CalendarLibs.createMonthDays(date);
-
-  // Temporary Dates
-  const now = useMemo(() => new Date(), []);
-  const diaries = useMemo(
-    () => [
-      new Date(2024, 10, 10),
-      new Date(2024, 10, 11),
-      new Date(2024, 10, 13),
-      new Date(2024, 10, 15),
-    ],
-    []
+  const className = ClassNameLibs.merge(
+    props,
+    "flex items-center px-5 h-[37px]"
   );
 
   return (
-    <div className="flex flex-col w-full max-w-[412px] px-5 whitespace-nowrap min-w-[280px]">
-      <div className="flex items-center px-3 h-[37px]">
-        <div className="w-full py-2 px-3 flex justify-between items-center">
-          {weekDays.map((day) => {
-            return (
-              <div key={day} className="flex justify-center items-center">
-                <span className="text-app-gray-1100 text-body3">{day}</span>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-      <div className="relative w-full flex flex-col p-3 gap-y-2.5 rounded-xl">
-        <div className="absolute bg-gradient-to-r from-gradient-01-100 to-gradient-01-200 opacity-10 rounded-xl top-0 left-0 w-full h-full" />
-        {matrix.map(({ array: row, key }) => {
+    <div className={className}>
+      <div className="w-full py-2 px-6 flex justify-between items-center">
+        {weekDays.map((day) => {
           return (
-            <div className="py-1 flex justify-between relative" key={key}>
-              {row.map((day) => {
-                const type = CalendarLibs.getCalendarType(day, {
-                  now,
-                  diaries,
-                  selectedDate,
-                  stage: CalendarStageType.DEFAULT,
-                });
-                return (
-                  <AppCalendarDay.Default
-                    day={day.date}
-                    key={day.key}
-                    type={type}
-                  />
-                );
-              })}
+            <div key={day} className="flex justify-center items-center">
+              <span className="text-white text-body3">{day}</span>
             </div>
           );
         })}
@@ -207,52 +136,63 @@ const AppCalendarBodyDefault = () => {
   );
 };
 
-const AppCalendarBodyCreate = () => {
-  const { date, selectedDate, onChange } = useContext(AppCalendarContext);
-  const weekDays = CalendarLibs.createWeekNames(date);
-  const { matrix } = CalendarLibs.createMonthDays(date);
+export interface AppCalendarBodyContainerProps extends PropsWithChildren {
+  className?: string;
+}
 
-  // Temporary Dates
-  const now = useMemo(() => new Date(), []);
+const AppCalendarBodyContainer = (props: AppCalendarBodyContainerProps) => {
+  const { children } = props;
+  const className = ClassNameLibs.merge(
+    props,
+    "flex flex-col w-full px-5 whitespace-nowrap min-w-[280px] transition-all"
+  );
+
+  return <div className={className}>{children}</div>;
+};
+
+export interface AppCalendarBodyProps {
+  children: CalendarDay;
+  className?: string;
+}
+
+const AppCalendarShortBody = (props: AppCalendarBodyProps) => {
+  const today = useMemo(() => new Date(), []);
+  const array = CalendarLibs.createWeekDays(today);
+  const { children } = props;
+
   return (
-    <div className="flex flex-col w-full max-w-[412px] px-5 whitespace-nowrap min-w-[280px]">
-      <div className="flex items-center px-3 h-[37px]">
-        <div className="w-full py-2 px-3 flex justify-between items-center">
-          {weekDays.map((day) => {
-            return (
-              <div key={day} className="flex justify-center items-center">
-                <span className="text-app-gray-1100 text-body3">{day}</span>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-      <div className="relative w-full flex flex-col p-3 gap-y-2.5 rounded-xl">
-        <div className="absolute bg-gradient-to-r from-gradient-01-100 to-gradient-01-200 opacity-10 rounded-xl top-0 left-0 w-full h-full" />
-        {matrix.map(({ array: row, key }) => {
-          return (
-            <div className="py-1 flex justify-between relative" key={key}>
-              {row.map((day) => {
-                const type = CalendarLibs.getCalendarType(day, {
-                  now,
-                  selectedDate,
-                  stage: CalendarStageType.DEFAULT,
-                });
-                return (
-                  <AppCalendarDay.Create
-                    day={day.date}
-                    key={day.key}
-                    type={type}
-                    onClick={() => {
-                      onChange?.(day.date);
-                    }}
-                  />
-                );
-              })}
-            </div>
-          );
+    <div className="relative w-full flex flex-col gap-y-2.5 rounded-xl">
+      <div className="absolute bg-gradient-to-r from-gradient-01-100 to-gradient-01-200 opacity-10 rounded-xl top-0 left-0 w-full h-full" />
+      <div className="py-1 flex justify-between relative backdrop-blur px-3 rounded-xl">
+        {array.map((day) => {
+          return cloneElement(children({ date: day }) as JSX.Element, {
+            key: day.key,
+          });
         })}
       </div>
+    </div>
+  );
+};
+
+const AppCalendarBody = (props: AppCalendarBodyProps) => {
+  const { children } = props;
+  const { date } = useContext(AppCalendarContext);
+  const { matrix } = CalendarLibs.createMonthDays(date);
+
+  return (
+    <div className="relative w-full flex flex-col p-3 gap-y-2.5 rounded-xl">
+      <div className="absolute bg-gradient-to-r from-gradient-01-100 to-gradient-01-200 opacity-10 rounded-xl top-0 left-0 w-full h-full backdrop-blur" />
+      {matrix.map(({ array: row, key }) => {
+        return (
+          <div className="py-1 flex justify-between relative" key={key}>
+            {row.map((day) => {
+              return cloneElement(children({ date: day }) as JSX.Element, {
+                key: day.key,
+              });
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 };
@@ -261,15 +201,16 @@ export interface AppCalendarProps extends PropsWithChildren {
   className?: string;
   close?: boolean;
 
-  date?: Date;
-  selectedDate?: Date;
+  defaultDate?: Date;
   onChange?: (date: Date) => unknown;
 }
 
-const AppCalendar = forwardRef<HTMLDivElement, AppCalendarProps>(
-  function AppCalendarManual(props, ref) {
-    const { selectedDate, onChange, close = false, children } = props;
-    const [date, setDate] = useState(startOfDay(props.date ?? new Date()));
+const AppCalendarContainer = forwardRef<HTMLDivElement, AppCalendarProps>(
+  function AppCalendarContainer(props, ref) {
+    const { onChange, close = false, children } = props;
+    const [date, setDate] = useState(
+      startOfDay(props.defaultDate ?? new Date())
+    );
     const className = ClassNameLibs.merge(
       props,
       "w-full flex flex-col items-center"
@@ -286,14 +227,20 @@ const AppCalendar = forwardRef<HTMLDivElement, AppCalendarProps>(
         return subMonths(date, 1);
       });
     };
+
+    const onMonthClick = (month: number) => {
+      setDate((date) => {
+        return setMonth(date, month);
+      });
+    };
     return (
       <AppCalendarContext.Provider
         value={{
-          date,
-          selectedDate,
+          date: date,
           onNextClick,
           onPrevClick,
           onChange,
+          onMonthClick,
           close,
         }}
       >
@@ -305,19 +252,57 @@ const AppCalendar = forwardRef<HTMLDivElement, AppCalendarProps>(
   }
 );
 
-const AppCalendarBody = Object.assign(
-  {},
-  {
-    Default: AppCalendarBodyDefault,
-    Create: AppCalendarBodyCreate,
-  }
-);
+export interface AppCalendarSheetProps {
+  className?: string;
+  children?: ReactNode;
+}
 
-export {
-  AppCalendar,
-  AppCalendarBody,
-  AppCalendarHeader,
-  AppCalendarShortBody,
+const AppCalendarSheet = (props: AppCalendarSheetProps) => {
+  const { children } = props;
+  const className = ClassNameLibs.merge(props, "w-full flex flex-col bg-black");
+  const { date, onMonthClick } = useContext(AppCalendarContext);
+  const from = startOfYear(date);
+  const months = Array.from({ length: 12 }, (_n, num) => {
+    const month = addMonths(from, 11 - num);
+    return { text: format(month, "yyyy년 MM월"), month: 11 - num };
+  });
+
+  return (
+    <Sheet>
+      <SheetTrigger>{children}</SheetTrigger>
+      <SheetContent side="bottom" className="bg-black rounded-t-xl border-0">
+        <SheetTitle className="hidden"></SheetTitle>
+        <div className={className}>
+          {months.map((month) => {
+            return (
+              <SheetClose
+                key={month.text}
+                onClick={() => {
+                  onMonthClick?.(month.month);
+                }}
+              >
+                <div className="w-full h-[56px] flex justify-center items-center">
+                  <h4 className="text-white text-sub4">{month.text}</h4>
+                </div>
+              </SheetClose>
+            );
+          })}
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
 };
+
+const AppCalendar = Object.assign(
+  {
+    Container: AppCalendarContainer,
+    Header: AppCalendarHeader,
+    WeekDay: AppCalendarWeekDay,
+    BodyContainer: AppCalendarBodyContainer,
+    ShortBody: AppCalendarShortBody,
+    Body: AppCalendarBody,
+  },
+  {}
+);
 
 export default AppCalendar;
