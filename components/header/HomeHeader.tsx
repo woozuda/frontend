f3fb817e8e81752154baf24e217fb6ab92348b62
nwleedev <nw.lee@outlook.com>
2024-11-28 20:40:45 +0900

    feat: headers

--- components/header/HomeHeader.tsx
+++ components/header/HomeHeader.tsx
@@ -1,35 +1,51 @@
 "use client";
 
 import CalendarIcon from "@/app/assets/icons/Calendar.svg";
+import LogoSvg from "@/app/assets/icons/Logo.svg";
+import { RefLibs } from "@/app/lib/ref";
+import { ClassNameLibs } from "@/lib/utils";
 import Link from "next/link";
-import { MouseEventHandler } from "react";
+import { forwardRef, MouseEventHandler } from "react";
 
 export interface HomeHeaderProps {
   date: string;
+  className?: string;
 
   onShare?: MouseEventHandler<HTMLButtonElement>;
   onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-const HomeHeader = (props: HomeHeaderProps) => {
+const HomeHeader = forwardRef<HTMLElement, HomeHeaderProps>(function HomeHeader(
+  props,
+  ref
+) {
   const { date, onClick } = props;
+  const className = ClassNameLibs.merge(
+    props,
+    "h-14 p-1 flex items-center justify-between text-app-gray-1300 sticky left-0 top-0"
+  );
 
   return (
-    <header className="h-14 p-1 flex items-center justify-between text-app-gray-1300">
+    <header className={className} ref={RefLibs.connect(ref)}>
       <Link className="p-3 flex justify-center items-center" href="/">
-        <span className="text-lg font-bold text-app-gray-1300">우주다</span>
+        <LogoSvg />
       </Link>
       <div className="flex items-center">
-        <p className="p-3 flex justify-center items-center text-sub3">{date}</p>
+        <Link
+          href={"/calendar"}
+          className="p-3 flex justify-center items-center text-sub3 text-white"
+        >
+          {date}
+        </Link>
         <button
-          className="p-3 flex justify-center items-center"
+          className="p-3 flex justify-center items-center text-white"
           onClick={onClick}
         >
-          <CalendarIcon />
+          <CalendarIcon className="text-white" />
         </button>
       </div>
     </header>
   );
-};
+});
 
 export default HomeHeader;
