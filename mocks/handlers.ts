370118f7664117e57667b991a47efd23e5cafee2
nwleedev <nw.lee@outlook.com>
2024-11-29 23:38:23 +0900

    fix: handlers

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -42,8 +42,7 @@ export const handlers = [
   //랜덤 커버 생성
   http.post("api/diary/random", () => {
     return HttpResponse.json({
-      url:
-        "https://upload.wikimedia.org/wikipedia/ko/thumb/a/a6/Pok%C3%A9mon_Pikachu_art.png/200px-Pok%C3%A9mon_Pikachu_art.png",
+      url: "https://upload.wikimedia.org/wikipedia/ko/thumb/a/a6/Pok%C3%A9mon_Pikachu_art.png/200px-Pok%C3%A9mon_Pikachu_art.png",
     });
   }),
   //다이어리 생성
@@ -60,4 +59,6 @@ export const handlers = [
         "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fspace%2F&psig=AOvVaw1tbz6L4Y99OUU9fp7lslO4&ust=1732825335125000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCICbuser_YkDFQAAAAAdAAAAABAK",
     });
   }),
+  ...diaries,
+  ...notes,
 ];
