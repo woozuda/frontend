7bd2e76d0b0fc3bbc80505287556a21c9cc1a5ef
nwleedev <nw.lee@outlook.com>
2024-12-04 22:16:47 +0900

    feat: diary type select sheet

--- app/diary/[id]/page.tsx
+++ app/diary/[id]/page.tsx
@@ -12,7 +12,7 @@ export default function Page({ params }: { params: { id: number } }) {
     return null;
   }
   return (
-    <div className="flex w-full flex-col relative gap-y-3 pb-3">
+    <div className="flex w-full flex-col relative gap-y-3 pb-5">
       <div className="w-full flex items-center justify-between h-10 px-5">
         <h5 className="text-app-gray-600 text-sub5">총 {data?.noteCount}개</h5>
         <button className="w-10 h-10 flex items-center justify-center">
