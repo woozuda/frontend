2d6f26992610373d93c20707c8d1d572e45798a5
nwleedev <nw.lee@outlook.com>
2024-12-11 02:42:19 +0900

    feat: hooks to get reports

---
+++ app/hooks/useReport.ts
@@ -0,0 +1,49 @@
+import { useQuery } from "@tanstack/react-query";
+import { useHttp } from "../contexts/http";
+import { ReportAPI } from "../http/report";
+import { ReportLibs } from "../lib/report";
+import { RetrospectEnums } from "../models/report";
+
+export interface UseReportProps {
+  startDate: Date;
+  endDate: Date;
+  type: RetrospectEnums;
+}
+
+const useReport = (props: UseReportProps) => {
+  const { startDate, endDate, type } = props;
+  const http = useHttp();
+  const reportApi = new ReportAPI(http);
+
+  return useQuery({
+    queryKey: [
+      "DIARY_ANALYSIS",
+      ReportLibs.toDateParam(startDate),
+      ReportLibs.toDateParam(endDate),
+      type,
+    ] as const,
+    queryFn: async ({ queryKey }) => {
+      const [, start, end, type] = queryKey;
+
+      switch (type) {
+        case RetrospectEnums.FOUR_FS: {
+          return reportApi.getReport4FS(start, end);
+        }
+        case RetrospectEnums.KTP: {
+          return reportApi.getReportKTP(start, end);
+        }
+        case RetrospectEnums.PMI: {
+          return reportApi.getReportPMI(start, end);
+        }
+        case RetrospectEnums.SCS: {
+          return reportApi.getReportSCS(start, end);
+        }
+        default: {
+          return reportApi.getReport4FS(start, end);
+        }
+      }
+    },
+  });
+};
+
+export default useReport;
