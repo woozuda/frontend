4f6d27a982c2986e02f7dbb7fd01e3ceba00afd4
nwleedev <nw.lee@outlook.com>
2024-12-12 17:36:18 +0900

    fix: fetch with cookies

--- app/diary/[id]/layout.tsx
+++ app/diary/[id]/layout.tsx
@@ -1,12 +1,13 @@
-import { DiaryAPI } from "@/app/http";
-import { Http } from "@/app/lib/http";
 import { PropsWithChildren } from "react";
 
 import { Toaster } from "@/components/ui/sonner";
 
+import { DiaryAPI } from "@/app/http";
+import { Http } from "@/app/lib/http";
 import { DiaryNote } from "@/app/models/diary";
 import getQueryClient from "@/app/query/client";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { cookies } from "next/headers";
 
 export interface LayoutProps extends PropsWithChildren {
   params: Promise<{ id: number }>;
@@ -14,8 +15,15 @@ export interface LayoutProps extends PropsWithChildren {
 
 export default async function Layout(props: LayoutProps) {
   const { children, params } = props;
+  const cookie = cookies();
+  const authorization = cookie.get("Authorization")?.value;
   const id = (await params).id;
-  const http = new Http();
+  const http = new Http({
+    credentials: "include",
+    headers: {
+      Cookie: `Authorization=${authorization}`,
+    },
+  });
   if (process.env.NEXT_PUBLIC_BASE_URL) {
     http.baseURL = process.env.NEXT_PUBLIC_BASE_URL;
   }
