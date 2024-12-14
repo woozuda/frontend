f01a1261e50c1266233363131d8a03bad70b7f7b
nwleedev <nw.lee@outlook.com>
2024-12-15 00:43:54 +0900

    fix: note card with thumbnail

--- app/pages/diary/DateNotes.tsx
+++ app/pages/diary/DateNotes.tsx
@@ -3,6 +3,7 @@
 import { ArrowLeftSvg } from "@/app/assets/icons";
 import useNotes from "@/app/hooks/useNotes";
 import { DiaryLibs } from "@/app/lib/diary";
+import { HTMLLibs } from "@/app/lib/html";
 import { HeaderV2 } from "@/components/header/v2";
 import ListCard from "@/components/ListCard";
 import { cn } from "@/lib/utils";
@@ -24,7 +25,7 @@ const DiaryDateNotes = () => {
       : "bg-transparent z-20";
 
   return (
-    <div className="w-full h-full max-w-[480px] flex flex-col bg-auth bg-cover bg-no-repeat bg-center bg-sky-950">
+    <div className="w-full min-h-full h-auto max-w-[480px] flex flex-col bg-auth bg-cover bg-no-repeat bg-center bg-sky-950">
       <div className="w-full h-full flex flex-col relative">
         <HeaderV2 className={cn(bgColor, "sticky left-0 top-0 right-0")}>
           <HeaderV2.Left>
@@ -49,6 +50,9 @@ const DiaryDateNotes = () => {
           <div className="flex flex-col w-full p-5 relative">
             <div className="flex flex-col w-full gap-y-4">
               {notes?.map((note) => {
+                const image = HTMLLibs.findThumbnail(
+                  HTMLLibs.createDocument(note.note.content.join(""))
+                );
                 return (
                   <Link
                     href={`/note/${note.type}/${note.note.id}`}
@@ -56,8 +60,9 @@ const DiaryDateNotes = () => {
                   >
                     <ListCard.Container>
                       <ListCard.Header.Default title={note.note.title} />
+                      {image && <ListCard.Thumbnail thumbnail={image} />}
                       <ListCard.Description html>
-                        {String(note.note.content)}
+                        {note.note.content.join("")}
                       </ListCard.Description>
                     </ListCard.Container>
                   </Link>
