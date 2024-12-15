9410fb1e7edcbd984414c921835e05a239ec67ca
nwleedev <nw.lee@outlook.com>
2024-12-15 17:10:58 +0900

    feat: show report by status

--- app/pages/report/Diary.tsx
+++ app/pages/report/Diary.tsx
@@ -1,8 +1,32 @@
+"use client";
+
+import useReportCreateState from "@/app/hooks/useReportCreateState";
+import useReportDiary from "@/app/hooks/useReportDiary";
+import { ReportLibs } from "@/app/lib/report";
+import { useSearchParams } from "next/navigation";
+import { isNil } from "ramda";
 import { DiaryResult } from "./result";
+import ReportSpinner from "./spinner";
+import { DiarySufficient } from "./sufficient";
 
 const DiaryReport = () => {
-  // return <DiaryInsufficient />
-  // return <DiarySufficient />
+  const searchParams = useSearchParams();
+  const [start, end] = ReportLibs.getPeriod(searchParams);
+  const { data, isFetching } = useReportDiary({
+    startDate: start,
+    endDate: end,
+  });
+  const mutationState = useReportCreateState({
+    type: "DIARY",
+  });
+
+  if (mutationState.length > 0 || isFetching) {
+    return <ReportSpinner />;
+  }
+
+  if (isNil(data)) {
+    return <DiarySufficient />;
+  }
   return (
     <div className="flex flex-col w-full px-5 py-1 pb-10">
       <DiaryResult />
--- app/pages/report/Report4FS.tsx
+++ app/pages/report/Report4FS.tsx
@@ -1,8 +1,35 @@
-import Report4FSResult from "./result/Report4FS";
+"use client";
+
+import useReportCreateState from "@/app/hooks/useReportCreateState";
+import useReportRetrospective from "@/app/hooks/useReportRetrospective";
+import { ReportLibs } from "@/app/lib/report";
+import { RetrospectEnums } from "@/app/models/report";
+import { useSearchParams } from "next/navigation";
+import { isNil } from "ramda";
+import { Report4FSResult } from "./result";
+import ReportSpinner from "./spinner";
+import { ReportSufficient } from "./sufficient";
 
 const Report4FSReport = () => {
-  // return <ReportInsufficient />
-  // return <ReportSufficient />
+  const searchParams = useSearchParams();
+  const [start, end] = ReportLibs.getPeriod(searchParams);
+  const type = ReportLibs.getRetrospectType(searchParams);
+  const { data, isFetching } = useReportRetrospective({
+    startDate: start,
+    endDate: end,
+    type,
+  });
+  const mutationState = useReportCreateState({
+    type: RetrospectEnums.FOUR_F_S,
+  });
+
+  if (mutationState.length > 0 || isFetching) {
+    return <ReportSpinner />;
+  }
+
+  if (isNil(data)) {
+    return <ReportSufficient />;
+  }
   return (
     <div className="flex flex-col w-full px-5 py-1 pb-10">
       <Report4FSResult />
