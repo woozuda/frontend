bb3b476fdaf9fdf45dd42894791796c72c2cf5f6
nwleedev <nw.lee@outlook.com>
2024-12-15 17:10:39 +0900

    feat: create reports

--- app/pages/report/sufficient/Report.tsx
+++ app/pages/report/sufficient/Report.tsx
@@ -1,11 +1,22 @@
 import { Star28Svg } from "@/app/assets/icons";
+import useReportCreate from "@/app/hooks/useReportCreate";
+import { ReportLibs } from "@/app/lib/report";
+import { useSearchParams } from "next/navigation";
 import SufficientItem from "./Item";
 
 const ReportSufficient = () => {
+  const { mutateAsync } = useReportCreate();
+  const searchParams = useSearchParams();
+  const type = ReportLibs.getRetrospectType(searchParams);
+  const [start, end] = ReportLibs.getPeriod(searchParams);
+
   return (
     <SufficientItem
       icon={<Star28Svg className="text-white" />}
       text="이번주 회고를 분석해 보세요"
+      onClick={() => {
+        mutateAsync({ startDate: start, endDate: end, type });
+      }}
     />
   );
 };
