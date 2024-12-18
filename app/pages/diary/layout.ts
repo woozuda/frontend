02600d498cd2ff1a2ae654b60fe60402147bbbd5
nwleedev <nw.lee@outlook.com>
2024-12-18 17:47:46 +0900

    fix: loading ui

--- app/pages/diary/layout.ts
+++ app/pages/diary/layout.ts
@@ -1,5 +1,6 @@
 import dynamic from "next/dynamic";
 
 const DiaryLayout = dynamic(() => import("./layout/Layout"));
+const DiaryLoadingLayout = dynamic(() => import("./layout/Loading"));
 
-export { DiaryLayout };
+export { DiaryLayout, DiaryLoadingLayout };
